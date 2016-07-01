0bf8a4b8d0f82f5a8613dd698a49c335b8f29696
Buntry <parkergriep@yahoo.com>
2016-06-30 15:08:13 -1000

    lab complete

--- spec/jukebox_spec.rb
+++ spec/jukebox_spec.rb
@@ -10,9 +10,38 @@ songs = [
 ]
 
 describe Song do
-
+    let(:s) { Song.new("Peaches") }
+    
+    it "knows its name" do
+        expect(s.name).to eq("Peaches")
+    end
 end
 
 describe Jukebox do
-
+    let(:j) { Jukebox.new(songs) }
+    
+    it "understands what isn't an approved command" do
+        expect(j.command(':)')).to eq("invalid command")
+    end
+    
+    it "can play a song" do
+        expect(j.command("play Tiga")).to eq("now playing Tiga")
+    end
+    
+    it "knows if its on or off" do
+        expect(j.on?).to eq(true || false)
+    end
+    
+    it "exits" do
+        expect(j.command("exit")).to eq("Goodbye, thanks for listening!")
+        expect(j.on?).to eq(false)
+    end
+    
+    it "helps" do 
+        expect(j.command("help")).to eq("Please select help, list, exit, or play.")
+    end
+    
+    it "lists" do
+        expect(j.command("list")).to include("The Cults - Abducted")
+    end
 end
